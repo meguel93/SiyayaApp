45e91913d6bf69ffbb59979f9832b302a53150de
Rome <meguel93@gmail.com>
2018-07-28 17:07:21 +0200

    Facebook intergration

--- app/src/main/java/com/example/valtron/siyayaapp/MainActivity.java
+++ app/src/main/java/com/example/valtron/siyayaapp/MainActivity.java
@@ -4,21 +4,35 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.valtron.siyayaapp.Common.Common;
 import com.example.valtron.siyayaapp.Model.User;
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,20 +46,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import dmax.dialog.SpotsDialog;
 import io.paperdb.Paper;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSignIn, btnRegister;
+    private static final int REQUEST_CODE = 1000;
+    Button btnContinue;
     RelativeLayout rootDriverLayout;
 
     FirebaseAuth auth;
     FirebaseDatabase db;
     DatabaseReference users;
 
-    TextView txtForgotPassword;
+    //TextView txtForgotPassword;
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -61,46 +79,165 @@ public class MainActivity extends AppCompatActivity {
                 .build());
         setContentView(R.layout.activity_main);
 
+        printKeyHash();
+
         Paper.init(this);
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
         users = db.getReference(Common.user_driver_tbl);
 
-        btnSignIn = (Button) findViewById(R.id.btnSignIn);
-        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnContinue = (Button) findViewById(R.id.btnContinue);
+
         rootDriverLayout =(RelativeLayout) findViewById(R.id.rootDriverLayout);
-        txtForgotPassword = (TextView) findViewById(R.id.txt_forgot_password);
 
-        txtForgotPassword.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                showForgotPwdDialog();
-                return false;
-            }
-        });
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showRegisterDialog();
+                signInWithPhone();
             }
         });
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSignIn();
-            }
-        });
+        /*{
+            String user = Paper.book().read(Common.user_field);
+            String pwd = Paper.book().read(Common.pwd_field);
 
-        String user = Paper.book().read(Common.user_field);
-        String pwd = Paper.book().read(Common.pwd_field);
-
-        if(user != null && pwd != null)
-            if(!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pwd))
-                autoLogin(user, pwd);
+            if (user != null && pwd != null)
+                if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(pwd))
+                    autoLogin(user, pwd);
+        }*/
     }
 
+    private void signInWithPhone() {
+        Intent intent = new Intent(MainActivity.this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                        AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE) {
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (result.getError() != null) {
+                Toast.makeText(this, "" + result.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                return;
+            } else if (result.wasCancelled()) {
+                Toast.makeText(this, "Login Cancelled", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                if(result.getAccessToken() != null) {
+                    final android.app.AlertDialog waitingDialog = new SpotsDialog(this);
+                    waitingDialog.show();
+                    waitingDialog.setMessage("Please wait...");
+                    waitingDialog.setCancelable(false);
+
+                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                        @Override
+                        public void onSuccess(Account account) {
+                            final String userPhone = account.getPhoneNumber().toString();
+
+                            users.orderByKey().equalTo(userPhone)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if(!dataSnapshot.child(userPhone).exists()) {
+                                                User user = new User();
+                                                user.setPhone(userPhone);
+                                                user.setName(userPhone);
+                                                user.setAvatarUrl("");
+                                                //user.setRates("0.0");
+
+                                                users.child(userPhone)
+                                                        .setValue(user)
+                                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                            @Override
+                                                            public void onSuccess(Void aVoid) {
+                                                                users.child(userPhone)
+                                                                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                                                                            @Override
+                                                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                                                Common.currentUser = dataSnapshot.getValue(User.class);
+
+                                                                                Intent homeIntent = new Intent(MainActivity.this, DriverHome.class);
+                                                                                startActivity(homeIntent);
+
+                                                                                waitingDialog.dismiss();
+                                                                                finish();
+                                                                            }
+
+                                                                            @Override
+                                                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                                                            }
+                                                                        });
+                                                            }
+                                                        }).addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                            }
+                                            else {
+                                                users.child(userPhone)
+                                                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                                                            @Override
+                                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                                Common.currentUser = dataSnapshot.getValue(User.class);
+
+                                                                Intent homeIntent = new Intent(MainActivity.this, DriverHome.class);
+                                                                startActivity(homeIntent);
+
+                                                                waitingDialog.dismiss();
+                                                                finish();
+                                                            }
+
+                                                            @Override
+                                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                        }
+
+                        @Override
+                        public void onError(AccountKitError accountKitError) {
+                            Toast.makeText(MainActivity.this, "" + accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    private void printKeyHash() {
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.valtron.siyayaapp",
+                    PackageManager.GET_SIGNATURES);
+
+            for(Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+/*
     private void autoLogin(String user, String pwd) {
 
         final SpotsDialog waitingDialog = new SpotsDialog(MainActivity.this);
@@ -385,4 +522,5 @@ public class MainActivity extends AppCompatActivity {
 
         dialog.show();
     }
+    */
 }
